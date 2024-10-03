8c0d81828972d0ba4a6d1e6fb4057584803ceb3f
unnkkks <ksusham11@mail.ru>
2024-10-03 21:43:39 +0300

    added visit non resident HIR

--- lirs.hpp
+++ lirs.hpp
@@ -14,7 +14,8 @@ class cache
 
     enum class location
     {
-        in,
+        in_stack,
+        in_list,
         out,
     };
 
@@ -30,68 +31,44 @@ class cache
     std::size_t count_lirs;
     std::size_t Llirs;
 
-    std::stack<element> lirs_stack;
-
+    std::list<element> lirs_stack;
     std::list<element> resident_HIR_collection;
 
     std::unordered_map<key_T, element> cache_storage;
 
-    void visit_LIR(element elem)
+    void visit_LIR(element* elem)
     {
-        element front_elem;
-        front_elem = lirs_stack.back();
-        while(front_elem->element_state == lir)
-        {
-            elem_hit_the_top(front_elem);
-            front_elem = lirs_stack.back();
-        }
+        lirs_stack.push_front(elem);
         stack_pruning(lirs_stack);
     }
 
-    void stack_pruning(stack_)
+    void stack_pruning()
     {
         element front_elem;
-        front_elem = stack_.back()
+        front_elem = lirs_stack.front()
         while(front_elem->element_state == resident_hir or front_elem->element_state = non_resident_hir)
         {
-            remove_data_blocks(front_elem);
-            front_elem = stack_.back();
+            remove_data_blocks();
+            front_elem = lirs_stack.front();
         }
 
     }
 
-    void elem_hit_the_top(element elem)
+    void remove_data_blocks()
     {
-        lirs_stack.remove(elem);
-        lirs_stack.push_front(elem);
-        //return???
-    }
-    void remove_data_blocks(element elem)
-    {
-        front_elem = lirs_stack.back();
+        front_elem = lirs_stack.front();
         lirs_stack.pop_back();
         front_elem->element_state = out;
-        //return??
     }
 
-    void visit_resident_HIR(element elem)
+    void visit_resident_HIR(element* elem)
     {
-        element front_elem;
-        front_elem = lirs_stack.back();
-        while(front_elem->element_state != resident_hir)
-        {
-            add_to_res_HIR_collection(front_elem);
-            remove_data_blocks(front_elem);
-            front_elem = lirs_stack.back();
-        }
 
-        elem_hit_the_top(front_elem);
-
-        if(lirs_stack.contains(front_elem))
+        if(elem->element_location == in_stack)
         {
             front_elem->element_state = lir;
-            front_elem = lirs_stack.back();
-            resident_HIR_collection.delete(front_elem);
+            front_elem = lirs_stack.front();
+            resident_HIR_collection.erase(front_elem);
         }
 
         stack_pruning(lirs_stack);
@@ -99,14 +76,29 @@ class cache
         {
             list_top = resident_HIR_collection.front();
             resident_HIR_collection.push_back(list_top);
+            list_top->location = in_stack;
         }
     }
 
-    void add_to_res_HIR_collection(element elem)
+    void visit_non_resident_HIR(element* elem)
     {
-        resident_HIR_collection.push_back(elem);
-        elem->element_state = resident_hir;
-    }
+        front_elem = resident_HIR_collection.front();
+        resident_HIR_collection.pop_front();
+        front_elem->element_state = non_resident_hir;
 
+        if (elem->element_location = in_stack)
+        {
+            elem->element_state = lir;
+            stack_bottom = lirs_stack.back();
+
+            resident_HIR_collection.push_back(stack_bottom);
+            stack_bottom->element_state = hir;
+            stack_bottom->element_location = in_list;
+        }
+        else
+        {
+            resident_HIR_collection.push_back(elem);
+        }
+    }
 
 };
