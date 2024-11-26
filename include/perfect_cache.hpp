6f3b1a8605d3e314c287a420837749a48690fe44
unnkkks <ksusham11@mail.ru>
2024-11-26 21:26:53 +0300

    improved perfect cache

--- include/perfect_cache.hpp
+++ include/perfect_cache.hpp
@@ -9,6 +9,35 @@
 #include <algorithm>
 #include <iostream>
 
+
+template <typename key_T>
+class occurrence_table
+{
+    std::unordered_map<key_T, std::deque<int>> table_;
+
+    public:
+            static constexpr int no_next = -1;
+
+            template<typename key_iter>
+            occurrence_table(key_iter begin, key_iter end)
+            {
+                std::for_each(begin, end,
+                [this, index = 0](const key_T &key) mutable{add(key, index++);});
+            }
+
+            void add(const key_T &key, int occurrence) {table_[key].push_back(occurrence);}
+
+            void pop_first(const key_T &key) {table_.at(key).pop_front();}
+
+            int first(const key_T &key) const
+            {
+                auto &deque = table_.at(key);
+                return deque.empty() ? no_next : deque.front();
+            }
+
+};
+
+
 template <typename key_T, typename page_T, typename page_getter>
 class perfect_cache
 {
@@ -20,13 +49,14 @@ class perfect_cache
     std::list<page_T> pages_;
     std::unordered_map<key_T, iterator> cache_storage_;
     std::deque<key_T> requests_;
+    occurrence_table<key_T> occurrence_table_;
 
 
      public:
 
         template<std::forward_iterator Iterator>
         perfect_cache(size_t capacity, page_getter slow_get_page, Iterator begin, Iterator end):
-            cache_capacity_{capacity}, slow_get_page_{slow_get_page}, requests_{begin, end}
+            cache_capacity_{capacity}, slow_get_page_{slow_get_page}, requests_{begin, end}, occurrence_table_{begin, end}
             {
                 for (size_t i = 0; i < std::min(capacity, requests_.size()); ++i){
                     pages_.emplace_back(slow_get_page_(requests_[i]));
@@ -43,19 +73,23 @@ class perfect_cache
 
             if (cache_storage_.contains(key)) return true;
 
-            auto request_next_occurrence = find_next_occurrence(key);
+            int requests_next_occurrence = occurrence_table_.first(key);
 
-            if (request_next_occurrence != no_later_occurrences)
+            if (requests_next_occurrence != occurrence_table<key_T>::no_next)
             {
                 if (full())
                 {
                     auto [iter, next_occurrence] = find_latest();
 
-                    if (next_occurrence != no_later_occurrences && next_occurrence < request_next_occurrence)
+                    if (next_occurrence != occurrence_table<key_T>::no_next && next_occurrence < requests_next_occurrence)
+                    {
+                        occurrence_table_.pop_first(key);
                         return false;
+                    }
                     else
                     {
                         pages_.erase(iter->second);
+                        occurrence_table_.pop_first(iter->first);
                         cache_storage_.erase(iter);
                     }
                 }
@@ -72,19 +106,6 @@ class perfect_cache
 
     private:
 
-        static constexpr int no_later_occurrences = -1;
-
-        int find_next_occurrence(const key_T& key)
-        {
-            auto iter = std::ranges::find(requests_, key);
-
-
-            if (iter == requests_.end())
-                return no_later_occurrences;
-            else
-                return std::distance(requests_.begin(), iter) + 1;
-        }
-
         std::pair<typename std::unordered_map<key_T, iterator>::iterator, int> find_latest()
         {
             int latest_occurrence = 0;
@@ -93,9 +114,9 @@ class perfect_cache
 
             for (auto end_iter = cache_storage_.end(); iter != end_iter; ++iter)
             {
-                int next_occurrence = find_next_occurrence(iter->first);
+                int next_occurrence = occurrence_table_.first(iter->first);
 
-                if (next_occurrence == no_later_occurrences)
+                if (next_occurrence == occurrence_table<key_T>::no_next)
                     return std::pair{iter, next_occurrence};
                 else if (next_occurrence > latest_occurrence)
                 {
